bd6fd9433b73a6afc0cd4fc5c6fcacb2991c470d
Emanuel Sales <emanuel.sales.060@gmail.com>
2026-04-05 13:47:28 -0300

    Add: showMenu()

---
+++ main.py
@@ -0,0 +1,30 @@
+
+tasksList = []
+
+def showMenu():
+    print("--- Task Tracker ---")
+    print("1. List tasks")
+    print("2. Add task")
+    print("3. Update task")
+    print("4. Delete task")
+    print("5. Close")
+    print("--------------------")
+    return input("Chose a option: ")
+
+while True:
+    
+    print(showMenu())
+    
+    option = showMenu
+    
+    match option:
+        case 1:
+            pass
+        case 2:
+            pass
+        case 3:
+            pass
+        case 4:
+            pass
+        case 5:
+            break
