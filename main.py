381189f55c0b0336ac9d8c606907c0f0804f1304
Emanuel Sales <emanuel.sales.060@gmail.com>
2026-04-05 21:54:59 -0300

    Add: gitiginore and Add_task function

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+import json
+from datetime import datetime
 
 tasksList = []
 
-def showMenu():
+def Get_Last_ID():
+    while True:
+        try:
+            with open("Python Challenges/Task Tracker CLI/IdCount.txt", "r+") as f:
+                lastId = f.read()
+                currentId = int(lastId) + 1
+            with open("Python Challenges/Task Tracker CLI/IdCount.txt", "w") as f:
+                f.write(str(currentId))
+                break
+        except FileNotFoundError:
+            with open("Python Challenges/Task Tracker CLI/IdCount.txt", "w") as f:
+                f.write("0")
+
+    return currentId
+
+def Show_Menu():
     print("--- Task Tracker ---")
     print("1. List tasks")
     print("2. Add task")
@@ -9,26 +26,44 @@ def showMenu():
     print("4. Delete task")
     print("5. Close")
     print("--------------------")
-    return int(input("Chose a option: "))
+    return input("Chose a option: ")
 
-
-
-while True:
-
-    option = showMenu()
-    print(option==int)
+def Add_Task():
+    currentId = Get_Last_ID()
+    currentTask = {
+        "id": currentId,
+        "title": str(input("Title: ")),
+        "description": str(input("Description: ")),
+        "stauts": "todo",
+        "createdAt": datetime.now().strftime("%Y-%m-%d %I:%M %p"),
+        "lastUpdateAt": datetime.now().strftime("%Y-%m-%d %I:%M %p")
+    }
+    with open("Python Challenges/Task Tracker CLI/TaskList.json", "a+") as f:
+        json.dump(currentTask, f, indent= 4)
+    print(f"Task added successfully! (Id {currentId})")
     
+while True:
+    
+    try:
+        option = int(Show_Menu())
+    except ValueError:
+        print("Invalid input. Try again...")
+        input()
+        option = 6
+        
     match option:
         case 1:
-            print(1)
+            pass
         case 2:
-            print(12)
+            Add_Task()
         case 3:
             pass
         case 4:
             pass
         case 5:
             break
+        case 6:
+            pass
         case _:
             print("Invalid input. Try again...")
             input()
